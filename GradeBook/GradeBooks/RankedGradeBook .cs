4559577a4559ddfc1e1eb0676aa1730b9ce09cc7
Mihlalee <mihlali.somi@krs.co.za>
2020-08-16 20:01:28 +0200

    Creating New GradeBookTypes
    added new classes

---
+++ GradeBook/GradeBooks/RankedGradeBook .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+using GradeBook.Enums;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GradeBook.GradeBooks
+{
+  public class RankedGradeBook : BaseGradeBook
+  {
+
+    public string Name { get; set; }
+
+    public RankedGradeBook(string name) : base(name)
+    {
+      Type = GradeBookType.Ranked;
+    }
+  }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+using GradeBook.Enums;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GradeBook.GradeBooks
+{
+  public class StandardGradeBook : BaseGradeBook
+  {
+
+    public string Name { get; set; }
+
+    public StandardGradeBook( string name)
+    {
+
+    }
+  }
+}
